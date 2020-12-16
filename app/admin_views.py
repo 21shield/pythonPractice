b781e99d1253c54dc8bf7ec8ddec0e11e4659b5e
21shield <21netaly@gmail.com>
2020-12-15 22:01:32 -0500

    added html admin files

--- app/admin_views.py
+++ app/admin_views.py
@@ -1,9 +1,10 @@
 from app import app
+from flask import render_template
 
 @app.route("/admin/dashboard")
 # when a user puts this is the url the following will be ran
 def admin_dashboard():
-    return "This is the admin files"
+    return render_template("admin/dashboard.html")
     
 @app.route("/admin/profile")
 def admin_profile():
--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
 from app import app
 
+from flask import render_template
+
 @app.route("/")
 # when a user puts this is the url the following will be ran
 def index():
-    return "Hello world! This is my first flask app"
-    
+    return render_template("public/index.html")
+
 @app.route("/about")
 def about():
     return "<h1 style = 'color:red'> ABOUT !!! <h1>"
